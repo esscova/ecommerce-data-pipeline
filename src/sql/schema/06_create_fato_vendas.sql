5bcac9ddbda5546c5a7662ff382d61d6d759e7f7
wellington moreira <wsantos08@hotmail.com>
2025-05-13 23:04:04 -0300

    refactor: retirar comentários

--- src/sql/schema/06_create_fato_vendas.sql
+++ src/sql/schema/06_create_fato_vendas.sql
@@ -1,6 +1,5 @@
 -- tabela fato
 CREATE TABLE IF NOT EXISTS FATO_VENDAS (
-    -- sk_venda SERIAL PRIMARY KEY, -- Comentado para mostrar a alternativa abaixo
     sk_produto INT NOT NULL REFERENCES DIM_PRODUTO(sk_produto),
     sk_vendedor INT NOT NULL REFERENCES DIM_VENDEDOR(sk_vendedor),
     sk_local INT NOT NULL REFERENCES DIM_LOCAL(sk_local),
@@ -9,13 +8,12 @@ CREATE TABLE IF NOT EXISTS FATO_VENDAS (
     preco_cents INTEGER,
     shipping_cost_cents INTEGER,
     purchase_rating INT,
-    etl_load_timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL, -- Vírgula aqui se sk_venda for a próxima coluna
-    sk_venda SERIAL PRIMARY KEY -- SEM vírgula aqui se for a última definição antes de fechar ')'
+    etl_load_timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    sk_venda SERIAL PRIMARY KEY
 );
 
--- Índices e Comentários ...
 
--- Índices em chaves estrangeiras da tabela fato são cruciais para performance de JOIN
+-- Índices para melhorar o desempenho
 CREATE INDEX IF NOT EXISTS idx_fato_vendas_sk_produto ON FATO_VENDAS(sk_produto);
 CREATE INDEX IF NOT EXISTS idx_fato_vendas_sk_vendedor ON FATO_VENDAS(sk_vendedor);
 CREATE INDEX IF NOT EXISTS idx_fato_vendas_sk_local ON FATO_VENDAS(sk_local);
